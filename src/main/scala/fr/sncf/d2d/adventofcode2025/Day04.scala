be5705ea1c46520085258ffaaa9ff591dbdda78f
AlbanPhelip <alban.phelip@gmail.com>
2025-12-04 09:29:40 +0100

    Day 4

---
+++ src/main/scala/fr/sncf/d2d/adventofcode2025/Day04.scala
@@ -0,0 +1,51 @@
+package fr.sncf.d2d.adventofcode2025
+
+import scala.util.{Success, Try}
+
+object Day04 extends AdventOfCode {
+
+  val parsedInput: Array[Array[Char]] = input.map(_.toCharArray)
+
+  private def countAdjacent(x: Int, y: Int): Int = {
+    Array(
+      Try(parsedInput(y - 1)(x)),
+      Try(parsedInput(y + 1)(x)),
+      Try(parsedInput(y - 1)(x - 1)),
+      Try(parsedInput(y + 1)(x - 1)),
+      Try(parsedInput(y - 1)(x + 1)),
+      Try(parsedInput(y + 1)(x + 1)),
+      Try(parsedInput(y)(x - 1)),
+      Try(parsedInput(y)(x + 1))
+    ).count(_ == Success('@'))
+  }
+
+  override def fileName: String = "aoc04.txt"
+
+  override def execute(): (Long, Long) = {
+
+    val star1 = parsedInput.indices.flatMap { y =>
+      parsedInput.head.indices.filter { x =>
+        parsedInput(y)(x) == '@' && countAdjacent(x, y) < 4
+      }
+    }.length
+
+    var finished = false
+    var star2 = 0
+
+    while(!finished) {
+      var localCount = 0
+      parsedInput.indices.flatMap { y =>
+        parsedInput.head.indices.map { x =>
+          if(parsedInput(y)(x) == '@' && countAdjacent(x, y) < 4) {
+            localCount += 1
+            parsedInput(y)(x) = '.'
+          }
+        }
+      }
+      star2 += localCount
+      if (localCount == 0) finished = true
+    }
+
+    (star1, star2)
+  }
+}
